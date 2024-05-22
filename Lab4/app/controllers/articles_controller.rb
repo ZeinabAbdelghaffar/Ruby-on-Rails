bdd8d0fdf1b5cd35e9a47434de3cd40328243f06
Zeinab Abdelghaffar <zeinanabdo88@gmail.com>
2024-05-22 16:45:16 +0300

    Ensure Lab 4 is added

--- Lab4/app/controllers/articles_controller.rb
+++ Lab4/app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "Zeinab", password: "123123123", only: :destroy
-  
+
   def index
     @articles = Article.all
   end
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -40,9 +40,15 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     if @article.destroy
-      redirect_to root_path, notice: "Article was successfully deleted."
+      redirect_to root_path, notice: 'Article was successfully deleted.'
     else
-      redirect_to root_path, alert: "Failed to delete the article."
+      redirect_to root_path, alert: 'Failed to delete the article.'
     end
   end  
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status, :image)
+  end
 end
